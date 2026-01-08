54378c007a1fff2dfd952c48aa1961404d355c3c
John <badaire.jonathan@gmail.com>
2026-01-08 09:03:21 +0000

    l'erreur persiste

--- src/components/app/map-component.tsx
+++ src/components/app/map-component.tsx
@@ -11,9 +11,9 @@ import type { Dealership } from '@/lib/types';
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-  iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
 
 
