95e2f20661b2a392e64029bac019981023d7e58a
John <badaire.jonathan@gmail.com>
2026-01-31 04:45:39 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/app/logo.tsx
+++ src/components/app/logo.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
+import { cn } from '@/lib/utils';
 
-const MotoTrustLogo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
-    <svg width="320" height="50" viewBox="0 0 320 50" xmlns="http://www.w3.org/2000/svg" className={className} {...props}>
-        <g fontFamily="Inter, sans-serif" fontWeight="800" fontSize="36" letterSpacing="-1.5">
-            
-            {/* MOTO part */}
-            <text x="0" y="38" fill="white">M</text>
-            
-            {/* First O (wheel) */}
-            <path d="M60,25 a15,15 0 1,0 -30,0 a15,15 0 1,0 30,0 zm-5,0 a10,10 0 1,1 -20,0 a10,10 0 1,1 20,0 z" fill="white"/>
-            
-            <text x="70" y="38" fill="white">T</text>
-            
-            {/* Second O (wheel) */}
-            <path d="M125,25 a15,15 0 1,0 -30,0 a15,15 0 1,0 30,0 zm-5,0 a10,10 0 1,1 -20,0 a10,10 0 1,1 20,0 z" fill="white"/>
-
-            {/* Moto outline */}
-            <path d="M 45,15 C 60,0, 90,5, 96,16 C 105,28, 90,30, 85,28 M 96,16 C 110,3, 130,12, 125,22" fill="none" stroke="#f97316" strokeWidth="4" strokeLinecap="round"/>
-            
-            {/* TRUST part */}
-            <text x="145" y="38" fill="#f97316">TRUST</text>
-            
-            {/* Sparkle */}
-            <path d="M285 42 L288 39 L291 42 L288 45 Z" fill="#f97316" />
-
-        </g>
-    </svg>
+const MotoTrustLogo: React.FC<React.SVGProps<SVGSVGElement>> = ({ className, ...props }) => (
+  <svg
+    viewBox="0 0 250 50"
+    xmlns="http://www.w3.org/2000/svg"
+    className={cn("h-10 w-auto", className)}
+    {...props}
+  >
+    <style>
+      {`
+        .logo-text { 
+          font-family: 'Inter', sans-serif;
+          font-weight: 800;
+          font-size: 30px;
+          letter-spacing: -1px;
+        }
+      `}
+    </style>
+    <text x="0" y="35" className="logo-text" fill="currentColor">
+      MOTO
+    </text>
+    <text x="95" y="35" className="logo-text" fill="hsl(var(--accent))">
+      TRUST
+    </text>
+  </svg>
 );
 
 export default MotoTrustLogo;
