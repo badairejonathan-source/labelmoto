659d2c087fe8d568250e48485d67e5653a5e1106
John <badaire.jonathan@gmail.com>
2026-03-03 08:36:42 +0000

    met a  jour  le  logo logo-moto

--- src/components/app/logo.tsx
+++ src/components/app/logo.tsx
@@ -7,7 +7,7 @@ const LabelMotoLogo = ({ className }: { className?: string }) => {
   return (
     <div className={cn('relative w-full', className)}>
       <Image 
-        src="/images/logo-moto.png?v=3" 
+        src="/images/logo-moto.png?v=4" 
         alt="Label Moto Logo" 
         width={250} 
         height={80}
