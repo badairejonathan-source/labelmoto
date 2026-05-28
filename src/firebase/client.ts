85d95e607fd16d79b5fd14cf6fd0111872d3f6a9
John <badaire.jonathan@gmail.com>
2026-05-28 07:48:23 +0000

    J’ai enfin une piste sérieuse.
    Le bug ne semble pas venir de la logique

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -2,12 +2,13 @@
 
 /**
  * @fileOverview Point d'entrée unique pour le SDK Firebase Client.
+ * Isolation stricte des exports pour éviter les fuites serveur.
  */
 
 export { useFirebase, useAuth, useFirestore, useFirebaseApp, useMemoFirebase, useUser } from './provider';
-export * from './firestore/use-collection';
-export * from './firestore/use-doc';
-export * from './non-blocking-updates';
-export * from './non-blocking-login';
-export * from './errors';
-export * from './error-emitter';
+export { useCollection } from './firestore/use-collection';
+export { useDoc } from './firestore/use-doc';
+export { setDocumentNonBlocking, addDocumentNonBlocking, updateDocumentNonBlocking, deleteDocumentNonBlocking } from './non-blocking-updates';
+export { initiateEmailSignIn, initiateEmailSignUp, initiateAnonymousSignIn } from './non-blocking-login';
+export { errorEmitter } from './error-emitter';
+export { FirestorePermissionError } from './errors';
